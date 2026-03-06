09d60244da322e7e8786d680a7700dcc0a89b2fc
Ola Ersson <ola.ersson@gmail.com>
2026-03-06 13:57:16 +0100

    #3 create product list component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image';
 import Hero from './components/hero';
+import ProductListing from './components/product-listing';
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <ProductListing />
     </main>
   );
 }
